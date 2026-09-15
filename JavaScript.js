c83490d7898aa91b65a7db477aa8a4805b918e4f
2511130117-jpg <2511130117@undc.edu.pe>
2026-09-15 12:18:44 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,13 @@
+const usuario = "Yo"
+let contador = 0;
+for (let i = 0; i < 3; i++) {
+    contador++;
+    console.log(contador);
+}
+console.log(usuario);
 
+function hablar(params) {
+    console.log("Buenas tardes");
+}
+hablar("Ruben");
+hablar("Roberto");
